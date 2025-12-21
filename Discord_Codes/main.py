9f83d3147e7e4561b0fea2526b4dc4887f8accae
Yaman Aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2025-12-21 19:54:26 -0400

    Update main.py

--- Discord_Codes/main.py
+++ Discord_Codes/main.py
@@ -15,7 +15,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True # REQUIRED to read messages!
+intents.message_content = True 
 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
