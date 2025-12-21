6ef08d6be9f9628f2a5556a4a0cfcf4c14aa98c2
Yaman Aljnadi <60922667+yaman-aljnadi@users.noreply.github.com>
2025-12-21 19:33:54 -0400

    Update main.py

--- Discord_Codes/main.py
+++ Discord_Codes/main.py
@@ -1,1 +1,47 @@
 # Starting Steps of the Bratan Bot
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+TOKEN = os.getenv("BRATAN_TOKEN")
+
+print(TOKEN)
+
+
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True # REQUIRED to read messages!
+
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'SUCCESS: Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def ping(ctx):
+    """Responds to !ping with Pong!"""
+    await ctx.send('Pong!')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+
+    if message.content.lower() == "hello":
+        await message.channel.send("Hello there! I am ready to be an AI.")
+
+bot.run(TOKEN)
